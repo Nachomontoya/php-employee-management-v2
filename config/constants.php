7c104f0b2c5f01f825eaf70027f38557c49eaf24
Nachomontoya <montoyanacho@gmail.com>
2021-07-21 15:17:31 +0200

    process

--- config/constants.php
+++ config/constants.php
@@ -1,1 +1,12 @@
 <?php
+
+$documentRoot = getcwd();
+define("BASE_PATH", $documentRoot);
+
+define("LIBS", BASE_PATH . '/libs');
+
+define("CONTROLLERS", BASE_PATH . '/controllers');
+
+define("VIEWS", BASE_PATH . '/views');
+
+define("MODELS", BASE_PATH . '/models');
---
+++ controllers/loginController.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once '../libs/classes/controller.php';
+
+echo 'hello';
+
+// class Login {
+
+//   function __construct(){
+//     // parent::__construct();
+//     // $this->view->render('');
+//     echo "Hello Login Controller";
+//   }
+// }
